614b873cdc67c1b7cce79f39b5d89a5b523981f8
pi11 <userge@list.ru>
2013-07-26 01:36:56 +0400

    Added basic setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='python-tagger',
+    version=0.1,
+    description='Module for extracting tags from text documents.',
+    long_description=open('README.rst').read(),
+    author='Alessandro Presta',
+    url='https://github.com/pi11/tagger',
+    download_url='https://github.com/pi11/tagger/archive/master.zip',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
